d593c56b395579d9d71c4447e18796b32f69d532
krishna <krishna.priawan@gmail.com>
2018-12-20 17:01:40 +0900

    initial commit, able to run the predefined config

---
+++ src/application/MainGui.java
@@ -0,0 +1,77 @@
+package application;
+	
+import java.io.File;
+
+import sas.adaptation.SASStart;
+import sas.services.log.Log;
+import application.utility.Utility;
+import application.view.controller.ApplicationController;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+
+public class MainGui extends Application {	
+	
+	static final String logFile="results"+File.separator+"log.csv";
+	static final String resultFile="results"+File.separator+"result.csv";
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+	    	Utility.createFile(logFile);
+	    	Utility.createFile(resultFile);
+	    	Log.initialize(logFile);
+
+			SASStart tasStart = new SASStart();
+ 
+			//System.out.println("Start services");
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(getClass().getResource("view/application.fxml"));
+			SplitPane pane=(SplitPane)loader.load();			
+			
+			ApplicationController controller=(ApplicationController)loader.getController();
+			controller.setPrimaryStage(primaryStage);
+			controller.setSasStart(tasStart);
+			controller.setCompositeService(tasStart.getPatientService());
+			controller.setProbe(tasStart.getPatientMonitor());
+			controller.setConfigurations(tasStart.getAdaptationEngines());
+			controller.setServiceRegistry(tasStart.getServiceRegistry());
+			
+			Scene scene=new Scene(pane);
+			scene.getStylesheets().add(getClass().getResource("view/application.css").toExternalForm());
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent arg0) {
+					Log.stop();
+		    		System.exit(0);
+				}
+			});
+
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Smart Ambulance System");
+			primaryStage.show();
+			
+			Screen screen = Screen.getPrimary();
+			Rectangle2D bounds = screen.getVisualBounds();
+
+			primaryStage.setX(bounds.getMinX());
+			primaryStage.setY(bounds.getMinY());
+			primaryStage.setWidth(bounds.getWidth());
+			primaryStage.setHeight(bounds.getHeight());
+	        
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+			
+	public static void main(String[] args) {
+	    launch(args);
+	}
+}
